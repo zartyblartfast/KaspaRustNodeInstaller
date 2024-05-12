d53b7ec10136f22a3006e5b5daf1d288e07d3c53
zartyblartfast <zarty_blartfast@yahoo.com>
2024-05-12 18:46:36 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,58 @@
+@echo off
+
+:: Check for administrator privileges
+net session >nul 2>&1
+if %errorlevel% == 0 (
+    echo Running with administrative privileges
+    rem powershell -File "C:\path\to\your\script.ps1"
+) else (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~0' -Verb runAs" >nul
+    exit
+)
+
+
+set "DIRECTORY=C:\KaspaNode"
+
+if not exist "%DIRECTORY%" (
+    mkdir "%DIRECTORY%"
+    echo Directory created: %DIRECTORY%
+) else (
+    echo Directory already exists: %DIRECTORY%
+)
+
+
+setlocal enabledelayedexpansion
+
+REM Define file URLs and their respective names
+set "FILE1=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/RustyKaspaInstall.ps1"
+set "NAME1=%DIRECTORY%\RustyKaspaInstall.ps1"
+
+set "FILE2=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/ascii_art.txt"
+set "NAME2=%DIRECTORY%\ascii_art.txt"
+
+set "FILE3=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/header.txt"
+set "NAME3=%DIRECTORY%\header.txt"
+
+set "FILE4=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/readme.md"
+set "NAME4=%DIRECTORY%\readme.md"
+
+
+REM Array of file names for easier access in loop (optional, for multiple files)
+set "FILES=1 2 3 4"
+
+for %%i in (%FILES%) do (
+    set "CUR_FILE=!FILE%%i!"
+    set "CUR_NAME=!NAME%%i!"
+    
+    echo Downloading !CUR_NAME! from !CUR_FILE!
+    curl -L -o "!CUR_NAME!" "!CUR_FILE!"
+    if !ERRORLEVEL! neq 0 (
+        echo Failed to download !CUR_NAME!
+    ) else (
+        echo Successfully downloaded !CUR_NAME!
+    )
+)
+
+endlocal
+pause
