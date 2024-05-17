151d492046fa4517cd825066e9552d98932eb2b5
zartyblartfast <zarty_blartfast@yahoo.com>
2024-05-17 16:38:11 +0100

    Update setupRunThis.bat

--- setupRunThis.bat
+++ setupRunThis.bat
@@ -28,20 +28,50 @@ REM Define file URLs and their respective names
 set "FILE1=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/RustyKaspaInstall.ps1"
 set "NAME1=%DIRECTORY%\RustyKaspaInstall.ps1"
 
-set "FILE2=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/ascii_art.txt"
-set "NAME2=%DIRECTORY%\ascii_art.txt"
+set "FILE2=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/CloneRustyKaspa.psm1"
+set "NAME2=%DIRECTORY%\CloneRustyKaspa.psm1"
 
-set "FILE3=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/header.txt"
-set "NAME3=%DIRECTORY%\header.txt"
+set "FILE3=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/header.psm1"
+set "NAME3=%DIRECTORY%\header.psm1"
 
-set "FILE4=https://github.com/zartyblartfast/KaspaRustNodeInstaller/raw/main/README.md"
-set "NAME4=%DIRECTORY%\readme.md"
+set "FILE4=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/installGit.psm1"
+set "NAME4=%DIRECTORY%\installGit.psm1"
 
-set "FILE5=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/RunKaspaNode.bat"
-set "NAME5=%DIRECTORY%\RunKaspaNode.bat"
+set "FILE5=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/InstallLLVM.psm1"
+set "NAME5=%DIRECTORY%\InstallLLVM.psm1"
+
+set "FILE6=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/InstallProtocolBuffers.psm1"
+set "NAME6=%DIRECTORY%\InstallProtocolBuffers.psm1"
+
+set "FILE7=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/InstallVisualStudioBuildTools.psm1"
+set "NAME7=%DIRECTORY%\InstallVisualStudioBuildTools.psm1"
+
+set "FILE8=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/InstallWasm32Target.psm1"
+set "NAME8=%DIRECTORY%\InstallWasm32Target.psm1"
+
+set "FILE9=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/InstallWasmPack.psm1"
+set "NAME9=%DIRECTORY%\InstallWasmPack.psm1"
+
+set "FILE10=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/RustInstaller.psm1"
+set "NAME10=%DIRECTORY%\RustInstaller.psm1"
+
+set "FILE10=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/Utility.psm1"
+set "NAME10=%DIRECTORY%\Utility.psm1"
+
+set "FILE11=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/ascii_art.txt"
+set "NAME11=%DIRECTORY%\ascii_art.txt"
+
+set "FILE12=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/header.txt"
+set "NAME12=%DIRECTORY%\header.txt"
+
+set "FILE13=https://github.com/zartyblartfast/KaspaRustNodeInstaller/raw/main/README.md"
+set "NAME13=%DIRECTORY%\readme.md"
+
+set "FILE14=https://raw.githubusercontent.com/zartyblartfast/KaspaRustNodeInstaller/main/RunKaspaNode.bat"
+set "NAME14=%DIRECTORY%\RunKaspaNode.bat"
 
 REM Array of file names for easier access in loop (optional, for multiple files)
-set "FILES=1 2 3 4 5"
+set "FILES=1 2 3 4 5 6 7 8 9 10 11 12 13 14"
 
 for %%i in (%FILES%) do (
     set "CUR_FILE=!FILE%%i!"
@@ -58,6 +88,6 @@ for %%i in (%FILES%) do (
 
 endlocal
 
-powershell -File "%DIRECTORY%\RustyKaspaInstall.ps1"
+rem powershell -File "%DIRECTORY%\RustyKaspaInstall.ps1"
 
 pause
