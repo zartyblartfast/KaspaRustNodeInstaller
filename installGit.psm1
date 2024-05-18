59787af4b6ec688dbc20f3b83de384e1f0ccc61c
zartyblartfast <zarty_blartfast@yahoo.com>
2024-05-18 20:47:26 +0100

    Update installGit.psm1

--- installGit.psm1
+++ installGit.psm1
@@ -1,17 +1,31 @@
-﻿
-#  install Git Module
+# Install Git Module
 function Install-Git {
-
     write-output "****** Install-Git Function ******"
 
-    $url = "https://github.com/git-for-windows/git/releases/download/v2.45.0.windows.1/Git-2.45.0-64-bit.exe"
-    $output = "$env:TEMP\Git-Installer.exe"
+    # Load configuration
+    $configFilePath = Join-Path -Path $rootFolder -ChildPath "install_config.json"
+    $config = Get-Content -Raw -Path $configFilePath | ConvertFrom-Json
+
+    $url = $config.Git.url
+    $output = "$env:TEMP\\Git-Installer.exe"
+
+    # Check if Git is already installed
+    try {
+        $gitVersion = git --version
+        Write-Output "Git is already installed: $gitVersion"
+        LogMessage "Git is already installed: $gitVersion"
+        return
+    } catch {
+        Write-Output "Git is not installed. Proceeding with installation..."
+        LogMessage "Git is not installed. Proceeding with installation..."
+    }
 
     LogMessage "Downloading Git installer..."
     try {
         Invoke-WebRequest -Uri $url -OutFile $output
     } catch {
         LogMessage "Error downloading Git installer: $_"
+        Write-Output "Error: Failed to download Git installer. Check the log for details."
         return
     }
     
@@ -20,8 +34,12 @@ function Install-Git {
         Start-Process -FilePath $output -Args "/VERYSILENT" -Wait -NoNewWindow
     } catch {
         LogMessage "Error installing Git: $_"
+        Write-Output "Error: Failed to install Git. Check the log for details."
         return
     }
 
+    LogMessage "Git installation completed successfully."
     Remove-Item -Path $output
 }
+
+Export-ModuleMember -Function Install-Git
