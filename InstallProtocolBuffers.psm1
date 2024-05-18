2a12aa6129e93803a0d1831e2aac1c0e9b94caaa
zartyblartfast <zarty_blartfast@yahoo.com>
2024-05-18 20:58:43 +0100

    Update InstallProtocolBuffers.psm1

--- InstallProtocolBuffers.psm1
+++ InstallProtocolBuffers.psm1
@@ -1,10 +1,13 @@
-﻿
 # Install Protocol Buffers Module
 function InstallProtocolBuffers {
-
     write-output "****** Install-ProtocolBuffers Function ******"
 
-    $protocExePath = Join-Path -Path $rootFolder -ChildPath "protoc-21.10-win64\bin\protoc.exe"
+    # Load configuration
+    $configFilePath = Join-Path -Path $rootFolder -ChildPath "install_config.json"
+    $config = Get-Content -Raw -Path $configFilePath | ConvertFrom-Json
+
+    $url = $config.ProtocolBuffers.url
+    $protocExePath = Join-Path -Path $rootFolder -ChildPath "protoc-21.10-win64\\bin\\protoc.exe"
     
     if (Test-Path $protocExePath) {
         Write-Output "Protocol Buffers is already installed."
@@ -13,7 +16,6 @@ function InstallProtocolBuffers {
         Write-Output "Protocol Buffers is not installed. Installing now..."
         LogMessage "Protocol Buffers is not installed. Installing now..."
 
-        $url = "https://github.com/protocolbuffers/protobuf/releases/download/v21.10/protoc-21.10-win64.zip"
         $output = Join-Path -Path $rootFolder -ChildPath "protoc-21.10-win64.zip"
         
         LogMessage "Downloading Protocol Buffers now..."
@@ -21,6 +23,7 @@ function InstallProtocolBuffers {
             Invoke-WebRequest -Uri $url -OutFile $output
         } catch {
             LogMessage "Error downloading Protocol Buffers: $_"
+            Write-Output "Error: Failed to download Protocol Buffers. Check the log for details."
             return
         }
         
@@ -31,6 +34,7 @@ function InstallProtocolBuffers {
             New-Item -ItemType Directory -Force -Path $extractPath
         } catch {
             LogMessage "Failed to create directory: $extractPath"
+            Write-Output "Error: Failed to create directory. Check the log for details."
             return
         }
 
@@ -39,13 +43,14 @@ function InstallProtocolBuffers {
             Expand-Archive -Path $output -DestinationPath $extractPath -Force
         } catch {
             LogMessage "Error extracting the ZIP file: $_"
+            Write-Output "Error: Failed to extract ZIP file. Check the log for details."
             return
         }
 
         Remove-Item -Path $output
     }
 
-    Add-ToSystemPath -pathToAdd (Join-Path -Path $rootFolder -ChildPath "protoc-21.10-win64\bin")
+    Add-ToSystemPath -pathToAdd (Join-Path -Path $rootFolder -ChildPath "protoc-21.10-win64\\bin")
 }
 
 Export-ModuleMember -Function InstallProtocolBuffers
