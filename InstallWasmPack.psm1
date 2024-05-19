98e185531ca2eded3067bf24ba3e1d5f5e2f54ef
zartyblartfast <zarty_blartfast@yahoo.com>
2024-05-19 19:01:42 +0100

    Update InstallWasmPack.psm1

--- InstallWasmPack.psm1
+++ InstallWasmPack.psm1
@@ -15,22 +15,22 @@ function InstallWasmPack {
         try {
             $wasmPackVersion = & $wasmPackExecutable --version
             Write-Output "wasm-pack is already installed: $wasmPackVersion."
-            & LogMessage "wasm-pack is already installed: $wasmPackVersion."
+            LogMessage -message  "wasm-pack is already installed: $wasmPackVersion." -logFile $logFile
         } catch {
             Write-Output "Error checking wasm-pack version: $_"
-            & LogMessage "Error checking wasm-pack version: $_"
+            LogMessage -message  "Error checking wasm-pack version: $_" -logFile $logFile
         }
     } else {
         Write-Output "wasm-pack is not installed. Installing now..."
-        & LogMessage "wasm-pack is not installed. Installing now..."
+        LogMessage -message  "wasm-pack is not installed. Installing now..." -logFile $logFile
         try {
             $cargoExecutable = Join-Path -Path $env:USERPROFILE -ChildPath ".cargo\bin\cargo.exe"
             $null = & $cargoExecutable install wasm-pack 2>&1
             Write-Output "wasm-pack installation attempted."
-            & LogMessage "wasm-pack installation attempted."
+            LogMessage -message  "wasm-pack installation attempted." -logFile $logFile
         } catch {
             Write-Output "wasm-pack installation failed: $_"
-            & LogMessage "wasm-pack installation failed: $_"
+            LogMessage -message  "wasm-pack installation failed: $_" -logFile $logFile
         }
     }
 }
