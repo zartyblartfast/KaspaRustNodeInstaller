1f5e6b8a7615fcade356a27e6c0721c62e778c5c
zartyblartfast <zarty_blartfast@yahoo.com>
2024-05-19 16:48:44 +0100

    Update Utility.psm1

--- Utility.psm1
+++ Utility.psm1
@@ -1,8 +1,11 @@
-﻿# Utility Module
+# Utility Module
 
 # Function to log messages to a log file
 function LogMessage {
-    param([string]$message)
+    param(
+        [string]$message,
+        [string]$logfile
+    )
     Add-Content -Path $logFile -Value "$(Get-Date -Format "yyyy-MM-dd HH:mm:ss"): $message"
 }
 
